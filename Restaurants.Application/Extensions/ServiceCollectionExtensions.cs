f3fc9a4fe4d034a0da9c94977df5f15fe54ae0a2
Bashimane Mamabolo <mamabolobashimane@gmail.com>
2024-09-01 01:19:28 +0200

    Implemented FluentValidation for validatinmg user Post verb

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
 using Restaurants.Application.Restaurants.Dtos;
-using Restaurants.Domain.Repositories;
+using Restaurants.Application.Restaurants.Validators;
 namespace Restaurants.Application.Extensions;
 
 public static class ServiveCollectionExtensions
@@ -11,5 +13,12 @@ public static class ServiveCollectionExtensions
         services.AddScoped<IRestaurantsService, RestaurantsService>();
         //services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
         services.AddAutoMapper(typeof(RestaurantsProfile));
+        //services.AddValidatorsFromAssembly(typeof(ServiceCollectionExtensions).Assembly)
+        //    .AddFluentValidationAutoValidation();
+        // Register validators from the assembly
+        services.AddValidatorsFromAssemblyContaining<CreateRestaurantDtoValidator>();
+
+        // Enable FluentValidation's automatic model validation
+        services.AddFluentValidationAutoValidation();
     }
 }
--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -4,23 +4,23 @@ namespace Restaurants.Application.Restaurants.Dtos;
 
 public class CreateRestaurantDto
 {
-    [StringLength(100, MinimumLength = 3)]
+    //[StringLength(100, MinimumLength = 3)]
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
-    [Required(ErrorMessage = "Insert a valid category")]
+    //[Required(ErrorMessage = "Insert a valid category")]
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
 
-    [EmailAddress(ErrorMessage = "Please provide a valid email address")]
+    //[EmailAddress(ErrorMessage = "Please provide a valid email address")]
     public string? ContactEmail { get; set; }
 
-    [Phone(ErrorMessage = "Please provide a valid phone number")]
+    //[Phone(ErrorMessage = "Please provide a valid phone number")]
     public string? ContactNumber { get; set; }
 
     // Address
     public string? City { get; set; }
     public string? Street { get; set; }
 
-    [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Please provide a valid postal code in the format: XX-XXX.")]
+    //[RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Please provide a valid postal code in the format: XX-XXX.")]
     public string? PostalCode { get; set; }
 }
