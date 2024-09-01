8df2185df6ce73183cddcaba8b2ab232d35ca111
Bashimane Mamabolo <mamabolobashimane@gmail.com>
2024-09-01 18:09:39 +0200

    Serilog implemetnted. warning, information and console formatting

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,6 +1,8 @@
 using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +12,29 @@ builder.Services.AddControllers();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Host.UseSerilog((context, configuration) =>
+    configuration
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
+
+);
 
 var app = builder.Build();
 
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
-await seeder.Seed();
+try
+{
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+    await seeder.Seed();
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "An error occurred during seeding.");
+}
+
 // Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -13,7 +13,7 @@ public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandl
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Creating a new restaurants");
+        logger.LogInformation("Creating a new restaurant {@Restaurant}", request);
         var restaurant = mapper.Map<Restaurant>(request);
         int id = await restaurantsRepository.Create(restaurant);
         return id;
