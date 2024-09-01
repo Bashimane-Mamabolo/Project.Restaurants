01576fdbac879face3a9e88d2489b7099018220f
Bashimane Mamabolo <mamabolobashimane@gmail.com>
2024-09-01 23:07:22 +0200

    Implemented new subentities endpoints for the dishes

--- Restaurants.API/Controllers/DishesController.cs
+++ Restaurants.API/Controllers/DishesController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Dishes.Commands.CreateDish;
+using Restaurants.Application.Dishes.Dto;
+using Restaurants.Application.Dishes.Queries.GetDishByIdForRestaurant;
+using Restaurants.Application.Dishes.Queries.GetDishesForRestaurant;
 
 
 namespace Restaurants.API.Controllers;
@@ -11,11 +14,25 @@ namespace Restaurants.API.Controllers;
 public class DishesController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
-    public async Task<IActionResult> CreateDish(int restaurantId, [FromBody] CreateDishCommand command)
+    public async Task<IActionResult> CreateDish([FromRoute] int restaurantId, [FromBody] CreateDishCommand command)
     {
         command.RestaurantId = restaurantId;
 
         await mediator.Send(command);
         return Created();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DishDto>>> GetAllForRestaurant([FromRoute] int restaurantId)
+    {
+        var dishes = await mediator.Send(new GetDishesForRestaurantQuery(restaurantId));
+        return Ok(dishes);
+    }
+
+    [HttpGet("{dishId}")]
+    public async Task<ActionResult<DishDto>> GetByIdForRestaurant([FromRoute] int restaurantId, [FromRoute] int dishId)
+    {
+        var dish = await mediator.Send(new GetDishByIdForRestaurantQuery(restaurantId, dishId));
+        return Ok(dish);
     }
 }
